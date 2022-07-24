2a9f4ecc0aa094d9dc48cc81f3359a82107a5bbb
Sarah <xxklintan123xx@gmail.com>
2022-07-24 07:17:43 +0200

    switch to crunch

--- tools/ugo.go
+++ tools/ugo.go
@@ -1,16 +1,14 @@
 package tools
 
 import (
-	"bytes"
 	"encoding/base64"
-	"encoding/binary"
 	"encoding/json"
 	"fmt"
-	"io"
 	"os"
 	"strings"
 
 	"github.com/JoshuaDoes/gotena/utils"
+	crunch "github.com/superwhiskers/crunch/v3"
 )
 
 type UGO struct {
@@ -27,7 +25,7 @@ type UgoTitle struct {
 type UgoLayout int
 type UgoAssetType int
 
-func FileMagic() [4]byte { return [4]byte{'U', 'G', 'A', 'R'} }
+func FileMagic() []byte { return []byte{'U', 'G', 'A', 'R'} }
 
 const (
 	TypeButton UgoAssetType = iota
@@ -46,6 +44,28 @@ type UgoAsset struct {
 	Index4 int          `json:"number4,omitempty"`
 }
 
+func PackUgoJson(path string) ([]byte, error) {
+	ugoJSON, err := os.ReadFile(path)
+	if err != nil {
+		fmt.Println("[UGO] Error reading UGO JSON:", err)
+		return nil, err
+	}
+
+	ugo, err := ParseUgo(ugoJSON)
+	if err != nil {
+		fmt.Println("[UGO] Error parsing UGO JSON:", err)
+		return nil, err
+	}
+
+	ugoBytes, err := ugo.Pack()
+	if err != nil {
+		fmt.Println("[UGO] Error packing UGO JSON:", err)
+		return nil, err
+	}
+
+	return ugoBytes, nil
+}
+
 func ParseUgo(data []byte) (*UGO, error) {
 	var ugo UGO
 	err := json.Unmarshal(data, &ugo)
@@ -57,22 +77,26 @@ func ParseUgo(data []byte) (*UGO, error) {
 }
 
 func (u *UGO) Pack() ([]byte, error) {
-	w := bytes.NewBuffer([]byte{})
+	next := make([]byte, 0)
+	w := crunch.NewBuffer(make([]byte, 0))
 
-	sections := uint32(0)
+	tableOfContents := crunch.NewBuffer(make([]byte, 0))
+	extraData := crunch.NewBuffer(make([]byte, 0))
 
-	tableOfContents := bytes.NewBuffer([]byte{})
-	extraData := bytes.NewBuffer([]byte{})
-
-	binary.Write(tableOfContents, binary.LittleEndian, []byte(fmt.Sprintf("0\t%d\n", u.Layout)))
+	next = []byte(fmt.Sprintf("0\t%d\n", u.Layout))
+	tableOfContents.Grow(int64(len(next)))
+	tableOfContents.WriteBytesNext(next)
 	labels := []string{}
 
 	for _, name := range u.Title.Names {
 		labels = append(labels, base64.StdEncoding.EncodeToString(utils.WriteUTF16String(name)))
 	}
 
-	binary.Write(tableOfContents, binary.LittleEndian, []byte(fmt.Sprintf("1\t0\t%s\t%s\t%s\t%s\t%s\n", labels[0], labels[1], labels[2], labels[3], labels[4])))
+	next = []byte(fmt.Sprintf("1\t0\t%s\t%s\t%s\t%s\t%s\n", labels[0], labels[1], labels[2], labels[3], labels[4]))
+	tableOfContents.Grow(int64(len(next)))
+	tableOfContents.WriteBytesNext(next)
 
+	sections := uint32(0)
 	if u.Assets != nil {
 		sections++
 
@@ -87,46 +111,70 @@ func (u *UGO) Pack() ([]byte, error) {
 
 		for len(tableOfContents.Bytes())%4 != 0 {
 			alignment := make([]byte, 4-len(tableOfContents.Bytes())%4)
-			tableOfContents.Write(alignment)
+			tableOfContents.Grow(int64(len(alignment)))
+			tableOfContents.WriteBytesNext(alignment)
 		}
 	}
 
-	if extraData.Len() > 0 {
+	if len(extraData.Bytes()) > 0 {
 		sections++
-
 		for len(extraData.Bytes())%4 != 0 {
 			alignment := make([]byte, 4-len(extraData.Bytes())%4)
-			extraData.Write(alignment)
+			extraData.Grow(int64(len(alignment)))
+			extraData.WriteBytesNext(alignment)
 		}
 	}
 
-	binary.Write(w, binary.LittleEndian, FileMagic())
-	binary.Write(w, binary.LittleEndian, sections)
+	w.Grow(4 + 4)
+	next = FileMagic()
+	w.WriteBytesNext(next)
+	w.WriteU32LENext([]uint32{sections})
 
-	if sections == 1 {
-		binary.Write(w, binary.LittleEndian, uint32(len(tableOfContents.Bytes())))
-	} else if sections == 2 {
-		binary.Write(w, binary.LittleEndian, uint32(len(tableOfContents.Bytes())))
-		binary.Write(w, binary.LittleEndian, uint32(len(extraData.Bytes())))
+	if sections > 0 {
+		w.Grow(4)
+		w.WriteU32LENext([]uint32{uint32(tableOfContents.ByteCapacity())})
 	}
-	binary.Write(w, binary.LittleEndian, tableOfContents.Bytes())
-	binary.Write(w, binary.LittleEndian, extraData.Bytes())
+	if sections > 1 {
+		w.Grow(4)
+		w.WriteU32LENext([]uint32{uint32(extraData.ByteCapacity())})
+	}
+	w.Grow(int64(tableOfContents.ByteCapacity()))
+	w.WriteBytesNext(tableOfContents.Bytes())
+	w.Grow(int64(extraData.ByteCapacity()))
+	w.WriteBytesNext(extraData.Bytes())
 
 	return w.Bytes(), nil
 }
 
-func writeButton(tableContents, extraData io.Writer, asset UgoAsset) {
-	binary.Write(tableContents, binary.LittleEndian, []byte(fmt.Sprintf("4\t%s\t%d\t%s\t%d\t%d\t%d\n", asset.Url, asset.Index1, base64.StdEncoding.EncodeToString(utils.WriteUTF16String(asset.Name)), asset.Index2, asset.Index3, asset.Index4)))
+func writeButton(tableContents, extraData *crunch.Buffer, asset UgoAsset) {
+	next := []byte(fmt.Sprintf("4\t%s\t%d\t%s\t%d\t%d\t%d\n", asset.Url, asset.Index1, base64.StdEncoding.EncodeToString(utils.WriteUTF16String(asset.Name)), asset.Index2, asset.Index3, asset.Index4))
+	tableContents.Grow(int64(len(next)))
+	tableContents.WriteBytesNext(next)
+
 	if strings.HasSuffix(asset.Url, ".ppm") {
 		ppmFile, err := os.ReadFile("services/web/routes/res/bokeh.ppm")
 		if err != nil {
 			return
 		}
 
-		extraData.Write(ppmFile[:1696])
+		extraData.Grow(1696)
+		extraData.WriteBytesNext(ppmFile[:1696])
 	}
+
+	if asset.Image != "" {
+		imageFile, err := os.ReadFile(asset.Image)
+		if err != nil {
+			return
+		}
+
+		extraData.Grow(int64(len(imageFile)))
+		extraData.WriteBytesNext(imageFile)
+	}
+
 }
 
-func writeCategory(w io.Writer, asset UgoAsset) {
-	binary.Write(w, binary.LittleEndian, []byte(fmt.Sprintf("2\t%s\t%s\t%d\n", asset.Url, base64.StdEncoding.EncodeToString(utils.WriteUTF16String(asset.Name)), asset.Index1)))
+func writeCategory(w *crunch.Buffer, asset UgoAsset) {
+	next := []byte(fmt.Sprintf("2\t%s\t%s\t%d\n", asset.Url, base64.StdEncoding.EncodeToString(utils.WriteUTF16String(asset.Name)), asset.Index1))
+	w.Grow(int64(len(next)))
+	w.WriteBytesNext(next)
 }
